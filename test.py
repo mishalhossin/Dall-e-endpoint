4c0eaa9bfc9be2df4a45f04e55171c705b8492fd
Mishal <91066601+mishalhossin@users.noreply.github.com>
2023-06-09 19:30:25 +0600

    Update test.py

--- test.py
+++ test.py
@@ -1,30 +1,29 @@
 import aiohttp
 import asyncio
 
+url = 'http://localhost:3000/text'  # Replace with the actual URL of the Flask application
+
 async def generate_response(prompt):
-    url = 'http://localhost:3000/text'  # Replace with the actual URL of the Flask application
-
-    payload = {
-        'prompt': prompt
-    }
-
-    try:
-        async with aiohttp.ClientSession() as session:
-            async with session.post(url, json=payload) as response:
-                response.raise_for_status()
-                data = await response.json()
-                generated_text = data.get('generated_text')
+    async with aiohttp.ClientSession() as session:
+        data = {'prompt': prompt}
+        async with session.post(url, json=data) as response:
+            if response.status == 200:
+                result = await response.json()
+                generated_text = result.get('generated_text')
                 return generated_text
-    except aiohttp.ClientError as e:
-        print(f"Error occurred: {e}")
-        return None
-      
-prompt = """
+            else:
+                return None
 
+# Example usage
+async def main():
+    prompt = """
 User: Hi there?
 Bot:
 """
+    generated_text = await generate_response(prompt)
+    if generated_text:
+        print(generated_text)
+    else:
+        print('Error generating response')
 
-response = asyncio.run(generate_response(prompt))
-
-print (response)
+asyncio.run(main())
